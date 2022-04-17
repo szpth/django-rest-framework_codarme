fcd0a90baa432d5b93522d0ed3b21220be5ac239
Helder Souza <heldisp@icloud.com>
2022-04-17 14:36:48 -0300

    API Listar e Detalhar

--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,29 @@
+from uuid import uuid4
+
+import phonenumbers
 from django.db import models
 
+
 # Create your models here.
+class StandardModelMixin(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False, verbose_name="ID")
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name="Created at")
+    updated_at = models.DateTimeField(auto_now=True, editable=False, verbose_name="Updated at")
+
+    class Meta:
+        abstract = True
+
+
+class Agendamento(StandardModelMixin):
+    data_horario = models.DateTimeField()
+    nome_cliente = models.CharField(max_length=255)
+    email_cliente = models.EmailField()
+    telefone_cliente = models.CharField(max_length=20)
+
+    @classmethod
+    def criar_agendamento(cls, telefone_cliente):
+        telefone_cliente = phonenumbers.parse(telefone_cliente, "BR")
+        format_telefone_cliente = phonenumbers.format_number(telefone_cliente, phonenumbers.PhoneNumberFormat.E164)
+        agendamento = Agendamento(telefone_cliente=format_telefone_cliente)
+        agendamento.save()
+        return
--- agenda/views.py
+++ agenda/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+
+from agenda.models import Agendamento
+from agenda.serializers import AgendamentoSerializer
+
 
 # Create your views here.
+@api_view(http_method_names=["GET"])
+def agendamento_detail(request, uuid):
+    obj = get_object_or_404(Agendamento, uuid=uuid)
+    serializer = AgendamentoSerializer(obj)
+
+    return JsonResponse(serializer.data)
+
+
+@api_view(http_method_names=["GET"])
+def agendamento_list(request):
+    qs = Agendamento.objects.all()
+    serializer = AgendamentoSerializer(qs, many=True)
+
+    return JsonResponse(serializer.data, safe=False)

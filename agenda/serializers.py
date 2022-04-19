27ce3803ee631c207015c502deafadd6a1f0c499
Helder Souza <heldisp@icloud.com>
2022-04-19 20:29:24 -0300

    feat: included schedule list route

--- agenda/serializers.py
+++ agenda/serializers.py
@@ -1,42 +1,58 @@
 import re
 
+from django.utils import timezone
 from rest_framework import serializers
 
 from agenda.models import Agendamento
 
 
-class AgendamentoSerializer(serializers.Serializer):
-    data_horario = serializers.DateTimeField()
-    nome_cliente = serializers.CharField(max_length=255)
-    email_cliente = serializers.EmailField()
-    telefone_cliente = serializers.CharField(max_length=20)
+class AgendamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Agendamento
+        fields = (
+            "data_horario",
+            "nome_cliente",
+            "email_cliente",
+            "telefone_cliente",
+        )
 
-    def create(self, validated_data):
-        is_e164 = Agendamento.to_e164(validated_data["telefone_cliente"])
-        if re.fullmatch(r"^[+][0-9]{13}", is_e164):
-            agendamento = Agendamento.objects.create(
-                data_horario=validated_data["data_horario"],
-                nome_cliente=validated_data["nome_cliente"],
-                email_cliente=validated_data["email_cliente"],
-                telefone_cliente=is_e164,
+    def validate_data_horario(
+        self, data_horario: timezone.datetime
+    ) -> timezone.datetime:
+        """Check if data_horario is in the past"""
+        if data_horario < timezone.now():
+            raise serializers.ValidationError(
+                "Agendamento não pode ser feito no passado!"
             )
-            return agendamento
 
-    def update(self, instance, validated_data):
-        instance.data_horario = validated_data.get(
-            "data_horario", instance.data_horario
-        )
-        instance.nome_cliente = validated_data.get(
-            "nome_cliente", instance.nome_cliente
-        )
-        instance.email_cliente = validated_data.get(
-            "email_cliente", instance.email_cliente
-        )
-        instance.telefone_cliente = Agendamento.to_e164(
-            validated_data.get("telefone_cliente", instance.telefone_cliente)
+        if data_horario.hour < 9 or data_horario.hour >= 18:
+            raise serializers.ValidationError(
+                "Agendamento não pode ser feito fora do horário de funcionamento!"
+            )
+
+        if data_horario.weekday() == 6:
+            raise serializers.ValidationError(
+                "Não é possível agendar no domingo!"
+            )
+
+        filter = Agendamento.objects.filter(data_horario=data_horario).filter(
+            cancelado=False
         )
 
-        # Check if phone is E.164 format
-        if re.fullmatch(r"^[+][0-9]{13}", instance.telefone_cliente):
-            instance.save()
-            return instance
+        if filter.exists():
+            raise serializers.ValidationError(
+                "Agendamento já existente para este horário!"
+            )
+
+        return data_horario
+
+    def validate_telefone_cliente(self, is_e164: str) -> str:
+        """Check if telefone_cliente is E.164 format"""
+        is_e164 = Agendamento.to_e164(is_e164)
+
+        if not re.fullmatch(r"^[+][0-9]{13}", is_e164):
+            raise serializers.ValidationError(
+                "Telefone inválido, informe um telefone celular com DDD."
+            )
+
+        return is_e164
--- agenda/views.py
+++ agenda/views.py
@@ -1,5 +1,8 @@
+from datetime import datetime, timedelta
+
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from django.utils import timezone
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -15,18 +18,11 @@ def agendamento_detail(request, uuid):
         return JsonResponse(serializer.data)
 
     if request.method == "PATCH":
-        serializer = AgendamentoSerializer(
-            obj, data=request.data, partial=True
-        )
+        serializer = AgendamentoSerializer(obj, data=request.data, partial=True)
         if serializer.is_valid():
-            try:
-                serializer.save()
-                return Response(status=201)
-            except AssertionError:
-                return JsonResponse(
-                    {"detail": "Informe um telefone celular válido com DDD."},
-                    status=400,
-                )
+            serializer.save()
+            return Response(status=201)
+        return JsonResponse(serializer.errors, status=400)
 
     if request.method == "DELETE":
         obj.cancelado = True
@@ -45,12 +41,60 @@ def agendamento_list(request):
         data = request.data
         serializer = AgendamentoSerializer(data=data)
         if serializer.is_valid():
-            try:
-                serializer.save()
-                return Response(status=201)
-            except AssertionError:
-                return JsonResponse(
-                    {"detail": "Informe um telefone celular válido com DDD."},
-                    status=400,
-                )
+            serializer.save()
+            return Response(status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+@api_view(http_method_names=["GET"])
+def horario_list(request):
+    if request.method == "GET":
+        data = request.query_params.get("data")
+        data = datetime.fromisoformat(data).date()
+        dt_tz = datetime(data.year, data.month, data.day, tzinfo=timezone.utc)
+        filter = list(
+            Agendamento.objects.filter(data_horario__date=dt_tz).filter(
+                cancelado=False
+            )
+        )
+
+        horarios = []
+
+        if dt_tz.weekday() == 6:
+            return JsonResponse(
+                {"response": "Não é possível agendar no domingo!"}, status=400
+            )
+
+        dt_hr_ini = datetime(
+            data.year, data.month, data.day, hour=9, tzinfo=timezone.utc
+        )
+
+        if dt_tz.weekday() == 5:
+            dt_hr_fim = datetime(
+                data.year, data.month, data.day, hour=13, tzinfo=timezone.utc
+            )
+        else:
+            dt_hr_fim = datetime(
+                data.year, data.month, data.day, hour=18, tzinfo=timezone.utc
+            )
+            int_ini = datetime(
+                data.year, data.month, data.day, hour=12, tzinfo=timezone.utc
+            )
+            int_fim = datetime(
+                data.year, data.month, data.day, hour=13, tzinfo=timezone.utc
+            )
+        delta = timedelta(minutes=30)
+
+        agendados = [x.data_horario for x in filter]
+
+        while dt_hr_ini < dt_hr_fim:
+            if dt_tz.weekday() == 5:
+                if dt_hr_ini not in agendados:
+                    horarios.append(dt_hr_ini.isoformat())
+            else:
+                if (dt_hr_ini not in agendados) and (
+                    dt_hr_ini < int_ini or dt_hr_ini >= int_fim
+                ):
+                    horarios.append(dt_hr_ini.isoformat())
+            dt_hr_ini += delta
+        return JsonResponse(horarios, safe=False)

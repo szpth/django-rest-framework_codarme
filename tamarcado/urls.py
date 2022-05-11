c6a2f0c0e23446a39c031d76e663e90b18c63259
Helder Souza <heldisp@icloud.com>
2022-05-10 23:23:49 -0300

    fix: django/heroku

--- tamarcado/urls.py
+++ tamarcado/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("", healthcheck),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include("agenda.urls")),

de4b90e7e601d13edfda462587d7e8d9a6a940c1
Helder Souza <heldisp@icloud.com>
2022-05-10 22:25:11 -0300

    make it better

--- tamarcado/settings/base.py
+++ tamarcado/settings/base.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG")
+DEBUG = os.environ.get("DJANGO_DEBUG", False)
 
 ALLOWED_HOSTS = []
 
